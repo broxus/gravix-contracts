b95ff8132815c90b72531b2d9b511284414f0a9f
Maga <steam.30mb1@gmail.com>
2023-01-28 03:10:11 +0300

    Up accounts onCodeUpgrade

--- scripts/0-get-codes.ts
+++ scripts/0-get-codes.ts
@@ -8,6 +8,9 @@ const main = async () => {
     const vault = await locklift.factory.getContractArtifacts('GravixVault');
     console.log('Vault\n', vault.code);
 
+    const acc = await locklift.factory.getContractArtifacts('GravixAccount');
+    console.log('Account\n', acc.code);
+
     const oracle = await locklift.factory.getContractArtifacts('OracleProxy');
     console.log('\nOracle\n', oracle.code);
 };
