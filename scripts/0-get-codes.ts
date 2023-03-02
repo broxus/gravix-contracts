f000593618b8ae261c6fe6d46271d4e6487f1d88
Maga <steam.30mb1@gmail.com>
2023-03-02 14:19:12 +0300

    Fixed bug with insurance fund overflow

--- scripts/0-get-codes.ts
+++ scripts/0-get-codes.ts
@@ -1,22 +1,17 @@
-const {isValidEverAddress} = require('../test/utils/common');
-const fs = require('fs');
-const prompts = require('prompts');
-const ora = require('ora');
-
-
 const main = async () => {
-    const vault = await locklift.factory.getContractArtifacts('GravixVault');
-    console.log('Vault\n', vault.code);
+  const vault = await locklift.factory.getContractArtifacts('GravixVault');
+  console.log('Vault\n', vault.code);
 
-    const acc = await locklift.factory.getContractArtifacts('GravixAccount');
-    console.log('Account\n', acc.code);
+  const acc = await locklift.factory.getContractArtifacts('GravixAccount');
+  console.log('\nAccount\n', acc.code);
 
-    const oracle = await locklift.factory.getContractArtifacts('OracleProxy');
-    console.log('\nOracle\n', oracle.code);
+  const oracle = await locklift.factory.getContractArtifacts('OracleProxy');
+  console.log('\nOracle\n', oracle.code);
 };
+
 main()
-    .then(() => process.exit(0))
-    .catch(e => {
-        console.log(e);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch(e => {
+    console.log(e);
+    process.exit(1);
+  });
