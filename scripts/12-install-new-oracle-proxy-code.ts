8821758637e0e9badb570ecd91b918824397459f
Maga <steam.30mb1@gmail.com>
2023-11-09 02:21:56 +0400

    Fixed upgrade in contracts

---
+++ scripts/12-install-new-oracle-proxy-code.ts
@@ -0,0 +1,49 @@
+import { toNano } from "locklift";
+import { isValidEverAddress } from "../test/utils/common";
+import { GravixVaultAbi } from "../build/factorySource";
+
+const prompts = require("prompts");
+const ora = require("ora");
+
+const main = async () => {
+    await locklift.deployments.load();
+    console.log("\x1b[1m", "\n\nUpgrade oracle proxy code:");
+    const response = await prompts([
+        {
+            type: "select",
+            name: "vault",
+            message: "Select vault to upgrade",
+            choices: [
+                { title: "prod", value: "prod_Vault" },
+                { title: "beta", value: "beta_Vault" },
+            ],
+            validate: (value: string) => (isValidEverAddress(value) ? true : "Invalid Everscale address"),
+        },
+    ]);
+    const manager = await locklift.deployments.getAccount("Manager").account;
+    console.log("\x1b[1m", `\nGet manager from deployments: ${manager.address}`);
+
+    const vault = await locklift.deployments.getContract<GravixVaultAbi>(response.vault);
+    console.log(`Get vault from deployments: ${vault.address}`);
+
+    const spinner = ora("Install new oracle proxy code...").start();
+
+    const acc_artifacts = await locklift.factory.getContractArtifacts("OracleProxy");
+    await locklift.tracing.trace(
+        vault.methods
+            .updateOracleProxyCode({
+                code: acc_artifacts.code,
+                meta: { sendGasTo: manager.address, callId: 0, nonce: 0 },
+            })
+            .send({ from: manager.address, amount: toNano(1) }),
+    );
+
+    spinner.succeed(`New oracle proxy code installed`);
+};
+
+main()
+    .then(() => process.exit(0))
+    .catch(e => {
+        console.log(e);
+        process.exit(1);
+    });
